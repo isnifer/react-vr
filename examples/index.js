fe09b85ab9264e4afce00d1ed20a5a6aa136bbae
Anton Kuznetsov <isnifer@gmail.com>
2016-10-07 13:16:13 +0300

    Update example

--- examples/index.js
+++ examples/index.js
@@ -1,98 +1,99 @@
-let assetpath = function(filename) { return 'assets/' + filename; };
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { BoxGeometry, ImageUtils, MeshBasicMaterial, Vector3, Quaternion} from 'three';
+import { Object3D, Mesh, AmbientLight, PointLight } from '../../react-three';
+import { Scene, KeyboardCamera, Sphere } from '../src/index';
 
-let React    = require('react');
-let ReactDOM = require('react-dom');
-let ReactVR  = require('../src/index');
-let THREE    = require('three');
+const assetpath = filename => `assets/${filename}`;
+const boxgeometry = new BoxGeometry(200,200,200);
+const cupcaketexture = ImageUtils.loadTexture( assetpath('cupCake.png') );
+const cupcakematerial = new MeshBasicMaterial( { map: cupcaketexture } );
 
-let { ReactTHREE } = ReactVR;
+const creamtexture  = ImageUtils.loadTexture( assetpath('creamPink.png') );
+const creammaterial = new MeshBasicMaterial( { map: creamtexture } );
 
-let boxgeometry     = new THREE.BoxGeometry(200,200,200);
-let cupcaketexture  = THREE.ImageUtils.loadTexture( assetpath('cupCake.png') );
-let cupcakematerial = new THREE.MeshBasicMaterial( { map: cupcaketexture } );
+const w = window.innerWidth;
+const h = window.innerHeight;
 
-let creamtexture  = THREE.ImageUtils.loadTexture( assetpath('creamPink.png') );
-let creammaterial = new THREE.MeshBasicMaterial( { map: creamtexture } );
-
-let w = window.innerWidth;
-let h = window.innerHeight;
-
-class Cupcake extends React.Component {
-  render() {
-    let position = this.props.position || new THREE.Vector3(0,0,0);
-    return <ReactTHREE.Object3D quaternion={this.props.quaternion} position={position}>
-      <ReactTHREE.Mesh position={new THREE.Vector3(0,-100,0)} geometry={boxgeometry} material={cupcakematerial} />
-      <ReactTHREE.Mesh position={new THREE.Vector3(0,100,0)} geometry={boxgeometry} material={creammaterial} />
-    </ReactTHREE.Object3D>
-  }
+class Cupcake extends Component {
+    render() {
+        const position = this.props.position || new Vector3(0, 0, 0);
+        return (
+            <Object3D quaternion={this.props.quaternion} position={position}>
+                <Mesh
+                    position={new Vector3(0, -100, 0)}
+                    geometry={boxgeometry}
+                    material={cupcakematerial}
+                />
+                <Mesh
+                    position={new Vector3(0, 100, 0)}
+                    geometry={boxgeometry}
+                    material={creammaterial}
+                />
+            </Object3D>
+        );
+    }
 }
 
 const KEY_CODES = {
-  V : 86
+    V: 86
 };
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      cupcakePosition : new THREE.Vector3(100,10,10),
-      renderVR        : true,
-      direction       : 1,
-    };
-
-    this.onKeyDown = this.onKeyDown.bind(this);
-  }
-
-  onKeyDown(event) {
-    if (event.keyCode === KEY_CODES.V) {
-      this.setState({ renderVR : !this.state.renderVR });
+    state = {
+        cupcakePosition: new Vector3(100, 10, 10),
+        renderVR: true,
+        direction: 1,
     }
-  }
 
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      let direction = this.state.direction;
-      let offset = 10 * direction;
-      let position = this.state.cupcakePosition.clone().setZ(this.state.cupcakePosition.z + offset);
-      if (this.state.cupcakePosition.z > 500) {
-        direction = -1;
-      }
-      else if (this.state.cupcakePosition.z < 0) {
-        direction = 1;
-      }
-      this.setState({
-        cupcakePosition : position,
-        direction       : direction,
-      });
+    onKeyDown = event => {
+        if (event.keyCode === KEY_CODES.V) {
+            this.setState({renderVR : !this.state.renderVR});
+        }
+    }
 
-    }, 20);
-    window.addEventListener( 'keydown', this.onKeyDown, false );
-  }
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            let direction = this.state.direction;
+            const offset = 10 * direction;
+            const cupcakePosition = this.state.cupcakePosition.clone().setZ(
+                this.state.cupcakePosition.z + offset
+            );
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-    window.removeEventListener( 'keydown', this.onKeyDown, false );
-  }
+            if (this.state.cupcakePosition.z > 500) {
+                direction = -1;
+            } else if (this.state.cupcakePosition.z < 0) {
+                direction = 1;
+            }
 
+            this.setState({direction, cupcakePosition});
 
-  render() {
-    let cupcakeProps = {
-      position: new THREE.Vector3(0,0,0),
-      quaternion: new THREE.Quaternion()
-    };
-    return <ReactVR.Scene renderVR={this.state.renderVR} width={w} height={h} camera='maincamera'>
-      <ReactVR.KeyboardCamera width={w} height={h} name='maincamera' />
-      <ReactTHREE.AmbientLight color={0xffffff} />
-      <ReactTHREE.PointLight />
-      <ReactVR.Sphere { ...cupcakeProps } />
-      <ReactVR.Sphere { ...cupcakeProps } position={ this.state.cupcakePosition } />
-    </ReactVR.Scene>;
-  }
+        }, 20);
+
+        window.addEventListener('keydown', this.onKeyDown, false);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+        window.removeEventListener('keydown', this.onKeyDown, false);
+    }
+
+    render() {
+        const cupcakeProps = {
+            position: new Vector3(0,0,0),
+            quaternion: new Quaternion(),
+        };
+
+        return (
+            <Scene renderVR={this.state.renderVR} width={w} height={h} camera="maincamera">
+                <KeyboardCamera width={w} height={h} name="maincamera" />
+                <AmbientLight color={0xffffff} />
+                <PointLight />
+                <Sphere {...cupcakeProps} />
+                <Sphere {...cupcakeProps} position={this.state.cupcakePosition} />
+            </Scene>
+        );
+    }
 }
 
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('content')
-);
+ReactDOM.render(<App />, document.getElementById('content'));
